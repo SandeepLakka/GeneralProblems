68facf7fef213d508a6b51a3b29eab5b6bb5ebc4
Sandeep Lakka <sandeep.lakka@netcracker.com>
2021-05-25 11:47:40 +0530

    Problem#3

---
+++ src/main/java/io/github/sandeeplakka/general/problems/ReverseNumber.java
@@ -0,0 +1,35 @@
+package io.github.sandeeplakka.general.problems;
+
+import java.util.Arrays;
+
+public class ReverseNumber {
+
+
+    private static final ReverseNumber self = new ReverseNumber();
+
+    public static void main(String[] args) {
+        int[] numbers = {123456, -980, 24608};
+        Arrays.stream(numbers).boxed().forEach(number ->
+                System.out.println("Number " + number
+                        + " reversed becomes : "
+                        + self.getReversedNumber(number))
+        );
+    }
+
+    /**
+     * Return number with all digits positions reversed.
+     * e.g: (-)123 will be converted to (-)321
+     *
+     * @param number Integer number
+     * @return Number with digits positions reversed
+     */
+    public int getReversedNumber(int number) {
+        int result = 0;
+        while (number != 0) {
+            result = result * 10 + number % 10;
+            number /= 10;
+        }
+        return result;
+    }
+
+}
