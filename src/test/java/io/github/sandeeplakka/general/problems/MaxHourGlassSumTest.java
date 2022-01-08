d955fc20d1e15aa6fff3de36ee6946cd8e1d1e72
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-01-08 21:12:45 +0530

    Added MaxHourGlassSum problem

---
+++ src/test/java/io/github/sandeeplakka/general/problems/MaxHourGlassSumTest.java
@@ -0,0 +1,136 @@
+package io.github.sandeeplakka.general.problems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxHourGlassSumTest {
+
+    private MaxHourGlassSum maxHourGlassSum;
+
+    @BeforeEach
+    public void init() {
+        maxHourGlassSum = new MaxHourGlassSum();
+    }
+
+    @Test
+    @DisplayName("Test 6X6 grid with all positives")
+    public void testA6By6GridWithAllPositives() {
+        //Not a good way to initialize collection, please excuse :)
+        List<List<Integer>> input = new ArrayList<>();
+        input.add(new ArrayList<Integer>() {{
+            add(1);
+            add(1);
+            add(1);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(0);
+            add(1);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(1);
+            add(1);
+            add(1);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(9);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(20);
+        }});
+
+        assertEquals(20, maxHourGlassSum.getMaxHourGlassSum(input));
+    }
+
+    @Test
+    @DisplayName("Test 6X6 grid with all negatives")
+    public void testA6By6GridWithAllNegatives() {
+        //Not a good way to initialize collection, please excuse :)
+        List<List<Integer>> input = new ArrayList<>();
+        input.add(new ArrayList<Integer>() {{
+            add(-1);
+            add(-1);
+            add(-1);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(0);
+            add(-1);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(-1);
+            add(-1);
+            add(-1);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(-9);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+        }});
+        input.add(new ArrayList<Integer>() {{
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(-20);
+        }});
+
+        assertEquals(0, maxHourGlassSum.getMaxHourGlassSum(input));
+    }
+
+
+}
