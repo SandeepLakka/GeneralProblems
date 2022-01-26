cd0d43ddb8a66e5a1ca74cbc082f5bd7e905a533
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-01-26 13:15:33 +0530

    Added memoized version of basic recursive implementation

--- src/main/java/io/github/sandeeplakka/general/problems/arrays/KnapSackProblem.java
+++ src/main/java/io/github/sandeeplakka/general/problems/arrays/KnapSackProblem.java
@@ -1,5 +1,7 @@
 package io.github.sandeeplakka.general.problems.arrays;
 
+import java.util.Arrays;
+
 public class KnapSackProblem {
 
     //classic 0-1 Knapsack problem implementation
@@ -22,5 +24,37 @@ public class KnapSackProblem {
                 //What happens if we don't take this
                 knapSackBasicRecursive(wt, val, W, n - 1)
         );
+    }
+
+
+    //classic 0-1 Knapsack problem implementation
+    //Memoized version of recursive solution
+    public int knapSackRecurMemoized(int[] wt, int[] val, int W, int n) {
+        //memo object
+        int[][] memo = new int[n + 2][W + 2];
+        Arrays.stream(memo).forEach(arr -> Arrays.fill(arr, -1));
+        return knapSackRecurMemoized(wt, val, W, n, memo);
+    }
+
+    private int knapSackRecurMemoized(int[] wt, int[] val, int W, int n, int[][] memo) {
+        //base case
+        if (n == 0 || W == 0) {
+            return 0;
+        }
+        if (memo[n][W] != -1) {
+            return memo[n][W];
+        }
+        if (wt[n - 1] > W) {
+            memo[n][W] = knapSackBasicRecursive(wt, val, W, n - 1);
+            return memo[n][W];
+        }
+        memo[n][W] = Math.max(
+                //What happens if we take this into our knapSack
+                val[n - 1] + knapSackBasicRecursive(wt, val, W - wt[n - 1], n - 1),
+                //What happens if we don't take this
+                knapSackBasicRecursive(wt, val, W, n - 1)
+        );
+        return memo[n][W];
+
     }
 }
