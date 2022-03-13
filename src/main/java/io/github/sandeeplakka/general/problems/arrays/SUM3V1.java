3a29f4acb1187d508d0438f39b342cef67240656
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-03-13 21:39:10 +0530

    Updated with better approach to 3SUM with duplicates.

--- src/main/java/io/github/sandeeplakka/general/problems/arrays/SUM3V1.java
+++ src/main/java/io/github/sandeeplakka/general/problems/arrays/SUM3V1.java
@@ -1,22 +1,60 @@
 package io.github.sandeeplakka.general.problems.arrays;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class SUM3V1 {
 
-//  Driver Code
-//    public static void main(String[] args) {
-//        SUM3V1 obj = new SUM3V1();
-//        int[] arr = {-1,0,1,2,-1,-4};
-//        int sum = 0;
-//        Arrays.stream(obj.naiveApproach(arr, sum)).map(Arrays::toString).forEach(System.out::println);
-//    }
+    //  Driver Code
+    public static void main(String[] args) {
+        SUM3V1 obj = new SUM3V1();
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        int sum = 0;
+        System.out.println("Naive Approach");
+        Arrays.stream(obj.naiveApproach(arr, sum)).map(Arrays::toString).forEach(System.out::println);
+        System.out.println("Better Approach");
+        System.out.println(obj.betterApproach(arr, sum));
+    }
     //classic 3SUM problem but with duplicate elements in input array
     //[-1,0,1,2,-1,-4] with sum = 0 --> [[-1,-1,2],[-1,0,1]]
 
-    //TODO better approach ( <n^3 time complexity ) implementation
-    public Set<String> betterApproach(int[] arr, int sum) {
-        return null;
+    public List<List<Integer>> betterApproach(int[] arr, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[] sortedArr = IntStream.of(arr).sorted().toArray();
+        int index = 0;
+        while (index < sortedArr.length) {
+            int curVal = sortedArr[index];
+            int complement = sum - curVal;
+            List<List<Integer>> values = find2SUM(sortedArr, index, index + 1, complement);
+            result.addAll(values);
+            while (index < sortedArr.length && sortedArr[index] == curVal) {
+                index++;
+            }
+        }
+        return result;
+    }
+
+    private List<List<Integer>> find2SUM(int[] arr, int root, int startIndex, int sum) {
+        Set<Integer> vals = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+        int index = startIndex;
+        while (index < arr.length) {
+            int curVal = arr[index];
+            int remainingVal = sum - curVal;
+            if (vals.contains(remainingVal)) {
+                result.add(returnList(arr[root], curVal, remainingVal));
+            } else {
+                vals.add(curVal);
+            }
+            while (index < arr.length && arr[index] == curVal) {
+                index++;
+            }
+        }
+        return result;
+    }
+
+    private List<Integer> returnList(int a, int b, int c) {
+        return Arrays.asList(a, b, c);
     }
 
     public int[][] naiveApproach(int[] arr, int sum) {
